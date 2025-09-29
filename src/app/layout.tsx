db175cf3ec773ac4f5d8a0bb0574aeec4f8f8db7
Mohammed Maaz <mohammedmaaz2623@gmai.com>
2025-09-30 00:52:01 +0530

    feat: add ProgressBarProvider component and integrate next-nprogress-bar for loading indicators

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TRPCReactProvider } from "@/trpc/client";
+import ProgressBarProvider from "@/components/progress-bar-provider.tsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,7 +37,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <ProgressBarProvider>{children}</ProgressBarProvider>
           </ThemeProvider>
         </TRPCReactProvider>
       </body>
---
+++ src/components/progress-bar-provider.tsx.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
+import React, { ReactNode } from "react";
+
+const ProgressBarProvider = ({ children }: {children: ReactNode}) => {
+  return (
+    <>
+      {children}
+      <ProgressBar
+        height="4px"
+        color="#fffd00"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+    </>
+  );
+};
+
+export default ProgressBarProvider;
