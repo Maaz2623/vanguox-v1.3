dda7a7147ca5207b545ec934d794f93b347a7e93
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-10-15 11:29:09 +0530

    modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,18 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "@/components/ui/sonner";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY!,
+  authDomain: "vanguox-702bb.firebaseapp.com",
+  projectId: "vanguox-702bb",
+  storageBucket: "vanguox-702bb.firebasestorage.app",
+  messagingSenderId: "416425412193",
+  appId: "1:416425412193:web:580636abe0b457c3a7cc20",
+  measurementId: "G-RZ58VKHV31",
+};
 
 export const metadata: Metadata = {
   title: "Vanguox - All-in-One AI Platform for all your tasks",
@@ -13,6 +25,21 @@ export const metadata: Metadata = {
       rel: "icon",
       url: "/logo.png",
     },
+  ],
+  keywords: [
+    "Vanguox",
+    "AI for daily tasks vanguox",
+    "productivity automation vanguox",
+    "task automation vanguox",
+    "AI tools vanguox",
+    "personal assistant AI vanguox",
+    "business productivity vanguox",
+    "workflow optimization vanguox",
+    "daily work AI solutions vanguox",
+    "AI for efficiency vanguox",
+    "time-saving AI vanguox",
+    "AI for teams vanguox",
+    "smart task management vanguox",
   ],
 };
 
@@ -26,6 +53,9 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${cairo.className} antialiased`}>
